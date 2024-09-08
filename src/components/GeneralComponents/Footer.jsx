7fa27171985f2b859fbdbb0b90422c465c0467c4
jib2004 <obasaajibola04@gmail.com>
2024-09-08 22:01:49 +0100

    worked on the footer images

--- src/components/GeneralComponents/Footer.jsx
+++ src/components/GeneralComponents/Footer.jsx
@@ -1,13 +1,15 @@
-import { Box,UnorderedList,ListItem,Link } from '@chakra-ui/react'
-import { FaInstagram,FaGithub,FaLinkedin  } from "react-icons/fa";
+import { Box,UnorderedList,ListItem,Link,Image } from '@chakra-ui/react'
+import instagram from '../../assets/instagram.png'
+import linkedin from '../../assets/linkedin.png'
+import github from '../../assets/github-sign.png'
 
 const Footer = () => {
   return (
     <Box h={'60px'} display={'flex'} alignItems={'center'}>
       <UnorderedList listStyleType={'none'} display={'flex'} alignItems={'center'} gap={'20px'}>
-        <ListItem><Link href='https://www.instagram.com/obasaajibola04/' target='_blank'><FaInstagram className="size-8"/></Link></ListItem>
-        <ListItem><Link href='https://github.com/jib2004' target='_blank'><FaGithub className="size-8" /></Link></ListItem>
-        <ListItem><Link href='https://www.linkedin.com/in/obasa-ajibola-005690242/' target='_blank'><FaLinkedin className="size-8" /></Link></ListItem>
+        <ListItem><Link href='https://www.instagram.com/obasaajibola04/' target='_blank'><Image src={instagram} className="size-8"/></Link></ListItem>
+        <ListItem><Link href='https://github.com/jib2004' target='_blank'><Image src={github} className="size-8" /></Link></ListItem>
+        <ListItem><Link href='https://www.linkedin.com/in/obasa-ajibola-005690242/' target='_blank'><Image src={linkedin} className="size-8" /></Link></ListItem>
       </UnorderedList>
       
     </Box>
